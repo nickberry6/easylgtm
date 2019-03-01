3b7c76052df1876765d001bd94a680624ee33c0f
Joshua Russell <joshuarussell@Joshuas-MacBook-Pro.local>
2019-02-28 21:11:30 -0500

    adding react router and caymanjs for image editing

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
 import React from 'react'
 import Header from './Header'
-import Main from './Main'
-import Footer from './Footer'
-
+import Home from './Home'
+import Create from './Create'
 import 'bootstrap/dist/css/bootstrap.css';
 
-const App = () => (
-  <div>
-    <Header />
-    <Main />
-    <Footer />
-  </div>
-)
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-export default App
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/create" component={Create} />
+      <Route path="/memes" component={Memes} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
+);
+
+const About = () => <h2>About Us</h2>;
+const Meme = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Memes = ({ match }) => (
+  <div>
+    <h2>Memes</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/all`}>All</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/some`}>Some</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Meme} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+
+export default App;
